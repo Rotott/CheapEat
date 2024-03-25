b77bfb98e50d25d2249b00482323bbdaa5deb7fe
HeidiWann <143599995+HeidiWann@users.noreply.github.com>
2024-03-25 21:19:33 +0100

    Add files via upload

---
+++ Client/src/model/Food.java
@@ -0,0 +1,14 @@
+package model;
+
+import javax.swing.*;
+
+public class Food {
+    private String nameOfFood;
+
+    private FooCategory[] typeOfFood;
+    private int costOfFood;
+    private Ingredient[] ingredients;
+    public Food() {
+
+    }
+}

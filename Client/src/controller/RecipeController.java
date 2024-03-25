1f8c34e36ac11228d5150db1982120de8fd5b938
StoraDatorn <heidi.wannman@hotmail.se>
2024-03-25 19:40:22 +0100

    new test

---
+++ Client/src/controller/RecipeController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import model.FooCategory;
+import model.Ingredient;
+
+import javax.swing.*;
+
+public class RecipeController {
+
+    public RecipeController() {
+    }
+    public void createNewRecipe (String instructions, ImageIcon imageOfRecipe, Ingredient[] ingredients, String nameOfFood, FooCategory[] typeOfFood) {
+
+
+    }
+
+}

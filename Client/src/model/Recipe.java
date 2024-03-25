dfe29026e89add70bedfc30d8412a3dbb2c74838
StoraDatorn <heidi.wannman@hotmail.se>
2024-03-25 18:11:01 +0100

    test

---
+++ Client/src/model/Recipe.java
@@ -0,0 +1,15 @@
+package model;
+
+import javax.swing.*;
+
+public class Recipe {
+    private String instructions;
+    private ImageIcon imageOfRecipe;
+    private Food dish;
+    private String recipeName;
+    private int recipeId;
+    private boolean isPrivate;
+
+    public Recipe() {
+    }
+}
